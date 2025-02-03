dfb3b4aeb6ad01ccec01e104607888ba45099e02
Mutahhar Ali Moazzam <l1f20bsse0267@ucpedu.pk>
2025-02-03 07:30:14 +0500

    created branch feature/home-page

--- repo-config.sh
+++ repo-config.sh
@@ -1,8 +1,8 @@
 echo "Goin to Configure Local Git"
 git branch -m develop
-git config --local user.name "imran ashraf"
-git config --local user.email zsaing.ucp@gmail.com
-git config --local core.editor notepad
+git config --local user.name "Mutahhar Ali Moazzam"
+git config --local user.email l1f20bsse0267@ucpedu.pk
+git config --local core.editor code
 touch .gitignore
 mkdir ./src ./styles
 touch ./src/index.html ./styles/index.css
